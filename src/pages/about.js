036ea2ed90e47599d3d01a91f9033f1d76918052
Barrett <Aliciawyse@users.noreply.github.com>
2019-01-19 14:08:02 -0500

    added header and footer

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,11 @@
 import React from "react"
+import Header from "../components/header"
+import Footer from "../components/footer"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <h1>About REFACTR.TECH</h1>
+    <Header headerText="About Us"/>
     <p>REFACTR.TECH is all about growing and showcasing powerful voices of marginalized people and allies in tech. For three days, REFACTR.TECH focuses on technology while creating a safe space or thoughtful and nuanced conversations around diversity, inclusion, and intersectionality in tech.</p>
+    <Footer/>
   </div>
 )
