fd393570f30a2628462941d55c509e8167973b61
Barrett <Aliciawyse@users.noreply.github.com>
2019-04-13 08:14:18 -0400

    Editing speaker card to handle airtable data properly.

--- src/components/SpeakerCard.js
+++ src/components/SpeakerCard.js
@@ -1,17 +1,17 @@
 import React from "react"
 
-export const SpeakerCard = ({speaker_name, company, role, headshot}) => (
+export const SpeakerCard = ({speaker_name, company, role, headshot, linkedIn, twitter}) => (
     <div className="col-xl-3 col-lg-3 col-md-4 col-sm-12">
         <div className="speakers xs-mb30">
             <div className="spk-img">
-                <img className="img-fluid" alt="trainer-img" src={headshot}/>
+                <img className="img-fluid" alt="trainer-img" src={headshot[0].url}/>
                     <ul>
-                        <li><a ><i className="fa fa-twitter" /></a></li>
-                        <li><a ><i className="fa fa-linkedin" /></a></li>
+                        <li><a href={twitter}><i className="fa fa-twitter" /></a></li>
+                        <li><a href={linkedIn}><i className="fa fa-linkedin" /></a></li>
                     </ul>
                 </div>
             <div className="spk-info">
-                <a  rel="noreferrer noopener" target="_blank">
+                <a  href={twitter} rel="noreferrer noopener" target="_blank">
                     <h3>{speaker_name}</h3>
                 </a>
                 <p>{role}</p>
