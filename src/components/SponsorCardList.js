fb2c152d44447d65586bf6250abdb04c773d1f40
Jennifer Bland <ratracegrad@gmail.com>
2019-11-17 20:21:11 -0500

    Added key to all the sponsors so it is not generating an error message since there was no key

--- src/components/SponsorCardList.js
+++ src/components/SponsorCardList.js
@@ -3,6 +3,6 @@ import { SponsorCard } from "./SponsorCard";
 
 export const SponsorCardList = ({ items }) => {
     return items.map(item => (
-        <SponsorCard {...item.node.data}  />
+        <SponsorCard {...item.node.data} key={item.node.data.company_name} />
     ))
 }
