a5e8e2cd025af0edbc9dc0a2f6a03e4fb282ae68
Erica Stanley <erica.stanley@gmail.com>
2019-12-01 02:39:55 -0500

    horizontally centered sponsors

--- src/components/SponsorTier.js
+++ src/components/SponsorTier.js
@@ -4,7 +4,7 @@ export const SponsorTier = ({data}) => {
     return (
         <span>
             <h2 className="text-center mt50">{data.title}</h2>
-            <div className="row">
+            <div className="row justify-content-center">
                 {data.sponsors.map((sponsor) => (
                     <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3" key={sponsor.company_name}>
                         <a href={sponsor.url} rel="noreferrer noopener" target="_blank">
