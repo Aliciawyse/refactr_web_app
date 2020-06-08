130cb730d13c8dac887e154849b4744f0a0997be
Erica Stanley <erica.stanley@gmail.com>
2020-06-08 10:33:01 -0400

    minor corrections to articles/ blog posts

--- src/pages/CovidUpdate.js
+++ src/pages/CovidUpdate.js
@@ -45,49 +45,45 @@ export default ({ data }) => (
                 <h2>A REFACTR.TECH Update</h2>
               </div>
             </div>
-            <p style={{color:'#333'}}>By <a href="https://twitter.com/angelmbanks">ANGEL BANKS</a><br />
+            <p style={{color:'#444'}}>By <a href="https://twitter.com/angelmbanks">ANGEL BANKS</a><br />
                June 7, 2020 | 9:29 PM</p>
-            <p style={{fontSize: '1.4em', color:'#333'}}>
+            <p style={{fontSize: '1.4em', color:'#444'}}>
             We hold the health and safety of our community above everything. At this time, we don’t believe we can offer a space for our attendees, 
             speakers, and sponsors to responsibly socially distance, so with the continued uncertainty around COVID-19, we’ve decided to move our 
             conference to next year, <strong>September 15-17, 2021</strong>. We greatly appreciate your patience and understanding and look forward to seeing you 
             when we can all safely meet together again.
             </p>
 
-            <p style={{fontSize: '1.4em', color:'#333'}}>
-            We have temporarily put a hold on ticket sales, please 
-            <a href="https://tech.us19.list-manage.com/subscribe?u=5ae8995a4bf065a3c7cd6ad50&id=e8f624f474">join our mailing list</a> 
-            to be notified when you can begin to purchase tickets for 2021. For our current ticket holders, we are offering them the option 
-            to forward their ticket to 2021, transfer it to a new attendee, or request a full refund. 
+            <p style={{fontSize: '1.4em', color:'#444'}}>
+            We have temporarily put a hold on ticket sales, please <a href="https://tech.us19.list-manage.com/subscribe?u=5ae8995a4bf065a3c7cd6ad50&id=e8f624f474">join our mailing list</a> to
+             be notified when you can begin to purchase tickets for 2021. For our current ticket holders, we are offering you the option 
+            to forward your ticket to 2021, transfer it to a new attendee, or request a full refund. 
             </p>
 
-            <p style={{fontSize: '1.4em', color:'#333'}}>
-            In the meantime...we are going to do our best to provide you with amazing online content from our speakers. We have the following 
+            <p style={{fontSize: '1.4em', color:'#444'}}>
+            In the meantime...we are going to do our best to provide you with amazing online content from our community of speakers and friends of REFACTR. We have the following 
             events planned for you in July:
             </p>
 
-            <p style={{fontSize: '1.4em', color:'#333'}}>
-            Join us 90's hit "<strong>Bad Boys</strong>" online with Netflix Party on July 12, 2020 at 3:00 pm (est). Sign up on Eventbrite to join us.
+            <p style={{fontSize: '1.4em', color:'#444'}}>
+            Join us and watch 90's hit "<strong>Bad Boys</strong>" online with Netflix Party on July 12, 2020 at 3:00 pm EDT. <a href="https://www.eventbrite.com/e/refactrtech-90s-netflix-party-bad-boys-tickets-105670057988">Sign up</a> on Eventbrite to join us.
             </p>  
 
-            <p style={{fontSize: '1.4em', color:'#333'}}>
-            In the meantime...we are going to do our best to provide you with amazing online content from our speakers. We have the following 
-            events planned for you in July:
-            </p>
+  
 
-            <p style={{fontSize: '1.4em', color:'#333'}}>
+            <p style={{fontSize: '1.4em', color:'#444'}}>
             Join us for the second episode of our rerun series, “<strong>Planet of the APIs</strong>” with special guest, <a href="https://twitter.com/HenriHelvetica">Henri Helvetica</a> on July 22, 2020 at 
-            7:00 pm (est). We’ll all watch Henri’s talk <a href="https://2019.refactr.tech/sessions/planet-of-the-apis">Planet of the APIs</a> from <a href="https://2019.refactr.tech/">REFACTR.TECH 2019</a> and then join us for a discussion with Henri. 
-            <a href="https://www.eventbrite.com/e/refactrtech-rerun-with-henri-helvetica-tickets-106943013432"RSVP here></a>.
+            7:00 pm EDT. We’ll all watch Henri’s talk <a href="https://youtu.be/ZhtUKBaw1ho">Planet of the APIs</a> from <a href="https://2019.refactr.tech/">REFACTR.TECH 2019</a> and
+            then join us for a discussion with Henri. <a href="https://www.eventbrite.com/e/refactrtech-rerun-with-henri-helvetica-tickets-106943013432">RSVP here</a>.
             </p>
 
-            <p style={{fontSize: '1.4em', color:'#333'}}>
-            You can view our first episode in this series, “<strong>DevOps 101: Automation without frustration!</strong>” with Diana Rodriguez on our 
-            <a href="https://www.youtube.com/watch?v=_ZTZWJ749kU&list=PLtsNj-uCrJ4bz4138pDluELbJ5Hz_shLw">YouTube channel</a>. We will be announcing 
+            <p style={{fontSize: '1.4em', color:'#444'}}>
+            You can view our first episode in this series, “<strong>DevOps 101: Automation without frustration!</strong>” with Diana Rodriguez on 
+            our <a href="https://www.youtube.com/watch?v=_ZTZWJ749kU&list=PLtsNj-uCrJ4bz4138pDluELbJ5Hz_shLw">YouTube channel</a>. We will be announcing 
             our online events for July soon, <a href="https://tech.us19.list-manage.com/subscribe?u=5ae8995a4bf065a3c7cd6ad50&id=e8f624f474">subscribe to our newsletter</a> for more information.
             </p>
 
-            <p style={{fontSize: '1.4em', color:'#333'}}>
+            <p style={{fontSize: '1.4em', color:'#444'}}>
             We want to thank the community, especially our speakers and sponsors, for their ongoing support and contributions. We also want to thank 
             our venue, the <a href="https://loudermilkcenter.com/">Loudermilk Conference Center</a> for working with us to reschedule to these new dates.
             </p>
--- src/pages/blacklivesmatter.js
+++ src/pages/blacklivesmatter.js
@@ -45,9 +45,9 @@ export default ({ data }) => (
                 <h2>Black Lives Matter.</h2>
               </div>
             </div>
-            <p style={{color:'#333'}}>By <a href="https://twitter.com/ericastanley">ERICA STANLEY</a><br />
+            <p style={{color:'#444'}}>By <a href="https://twitter.com/ericastanley">ERICA STANLEY</a><br />
                June 1, 2020 | 9:17 PM</p>
-            <p style={{fontSize: '1.4em', color:'#333'}}>
+            <p style={{fontSize: '1.4em', color:'#444'}}>
             We at REFACTR.TECH stand unequivocally with Black people, #BlackLivesMatter protestors and those advocating for justice for Black people. 
             In the wake of a seemingly incessant stream of tragic events over a span of weeks — weeks that saw a long-delayed response to 
             Ahmaud Arbery’s murder, Breonna Taylor’s killing in her home at the hands of police, MPD’s use of excessive force in the killing of 
@@ -59,10 +59,10 @@ export default ({ data }) => (
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </p>
 
-            <p style={{fontSize: '1.4em', color:'#333'}}>My father grew up during the ‘50s and ‘60s. His was the first generation of Black students to integrate schools in the South. He was a teenager when Malcolm X was assassinated. He was a young adult when Martin Luther King Jr. was assassinated. He protested. His family and community leaders marched in pursuit of Civil Rights. Heartbroken over recent events, he told me they did that then so their children and grandchildren wouldn’t have to. And yet here we are, in 2020, fighting to just be, to exist without fear of becoming the next hashtag. If a “riot is the language of the unheard <sup>1</sup>,” know that we hear you and we amplify your voices.
+            <p style={{fontSize: '1.4em', color:'#444'}}>My father grew up during the ‘50s and ‘60s. His was the first generation of Black students to integrate schools in the South. He was a teenager when Malcolm X was assassinated. He was a young adult when Martin Luther King Jr. was assassinated. He protested. His family and community leaders marched in pursuit of Civil Rights. Heartbroken over recent events, he told me they did that then so their children and grandchildren wouldn’t have to. And yet here we are, in 2020, fighting to just be, to exist without fear of becoming the next hashtag. If a “riot is the language of the unheard <sup>1</sup>,” know that we hear you and we amplify your voices.
             </p>
 
-            <p style={{fontSize: '1.4em', color:'#333'}}>We know that protests and civic unrest can bring about change. The founding of our country was built on such unrest. As we begin Pride Month, I’m reminded of how the Stonewall riots, led primarily by Black and Latinx youth, ushered in a new era in the fight for LGBTQ+ rights. So as you demand justice in the streets across America, we ask that you do your best to preserve your health and your safety. 
+            <p style={{fontSize: '1.4em', color:'#444'}}>We know that protests and civic unrest can bring about change. The founding of our country was built on such unrest. As we begin Pride Month, I’m reminded of how the Stonewall riots, led primarily by Black and Latinx youth, ushered in a new era in the fight for LGBTQ+ rights. So as you demand justice in the streets across America, we ask that you do your best to preserve your health and your safety. 
             </p>
             
             <p className="text-center">
@@ -74,13 +74,13 @@ export default ({ data }) => (
               height="600"
             />
           </p>
-            <p style={{fontSize: '1.4em', color:'#333'}}>For those of you who may not be able to join the many protests happening throughout the country, there’s still plenty more work to be done. Check out the “Additional Reading” list below for ideas on how you can contribute to this moment.
+            <p style={{fontSize: '1.4em', color:'#444'}}>For those of you who may not be able to join the many protests happening throughout the country, there’s still plenty more work to be done. Check out the “Additional Reading” list below for ideas on how you can contribute to this moment.
             </p>
 
-            <p style={{fontSize: '1.4em', color:'#333'}}>REFACTR.TECH has, in our minds, always been more than a tech conference. It has been our way of fostering a tech industry that doesn’t exist yet—a tech industry that is inclusive, at all levels, of the wide range of diversity we see in the people who use the products we build. It has been our way to protest the tech companies, communities and events that pay little more than lip service to inclusion and intersectionality. It has been our way to highlight those who would typically be relegated to the margins and lift up their voices—to show the tech industry who we could be together. Showing our unwavering, uncompromising support for Black people, not only at this crucial juncture, but at all times, is who we are. 
+            <p style={{fontSize: '1.4em', color:'#444'}}>REFACTR.TECH has, in our minds, always been more than a tech conference. It has been our way of fostering a tech industry that doesn’t exist yet—a tech industry that is inclusive, at all levels, of the wide range of diversity we see in the people who use the products we build. It has been our way to protest the tech companies, communities and events that pay little more than lip service to inclusion and intersectionality. It has been our way to highlight those who would typically be relegated to the margins and lift up their voices—to show the tech industry who we could be together. Showing our unwavering, uncompromising support for Black people, not only at this crucial juncture, but at all times, is who we are. 
             </p> 
 
-            <p style={{color:'#333'}}>
+            <p style={{color:'#444'}}>
             <h3>Additional Reading</h3>
 
             <p style={{fontSize: '1.4em'}}>
