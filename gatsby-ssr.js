059091358d52c0e0f2263a7f5f92158f1d6280ca
Erica Stanley <erica.stanley@gmail.com>
2021-08-10 10:40:40 -0400

    added bootstrap back because of tab elements

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from "react";
 export const onRenderBody = ({
-  setHeadComponents,
+  setHeadComponents, setPostBodyComponents
 }) => {
   setHeadComponents([
     React.createElement('script', {
@@ -12,5 +12,29 @@ export const onRenderBody = ({
  slscout(["init", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0Ijo5MzI3fQ._yWESyK1NcerVYm4znug5rNzMBGdLuW6WkY08msspak"]);`
       }
     })
+  ])
+  
+  setPostBodyComponents([
+    <script
+      key="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+      integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+      crossOrigin="anonymous"
+      defer
+    />,
+    <script
+      key="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+      src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+      integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+      crossOrigin="anonymous"
+      defer
+    />,
+    <script
+      key="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+      src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+      integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+      crossOrigin="anonymous"
+      defer
+    />
   ]);  
 }  
