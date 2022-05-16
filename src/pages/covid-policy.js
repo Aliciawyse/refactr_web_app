e80c2c2df2725ce7b2d3e0129a4ff81c9fe42655
Erica Stanley <erica.stanley@gmail.com>
2022-05-16 02:26:55 -0400

    fixed naviagation error

--- src/pages/covid-policy.js
+++ src/pages/covid-policy.js
@@ -31,7 +31,7 @@ export default () => (
                 </div>
                 <ol className="breadcrumb">
                   <li>
-                    <a href="index.html">Home</a>
+                    <a href="/">Home</a>
                   </li>
                   <li>|</li>
                   <li>COVID-19 Policy</li>
