a297a161410e5be0b7354e6d549f1d2b152e9313
Erica Stanley <erica.stanley@gmail.com>
2022-08-23 14:24:08 -0400

    moar tracking

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
+          "G-DPQL2M21DH",
           "AW-854600055"],
         // This object is used for configuration specific to this plugin
         pluginConfig: {
